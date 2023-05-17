d07e45358ef2e6bcdee5302faf482279f6e88834
flaptop23 <awittung@2ndwatch.com>
2023-05-16 23:17:32 -0700

    nightly commit

---
+++ aws_delete_ami.py
@@ -0,0 +1,34 @@
+import boto3
+
+def describe_ami_snapshots(ami_id, profile_name):
+    session = boto3.Session(profile_name=profile_name)
+    ec2_client = session.client('ec2')
+    response = ec2_client.describe_images(ImageIds=[ami_id])
+    snapshots = []
+    for block_device in response['Images'][0]['BlockDeviceMappings']:
+        if 'Ebs' in block_device:
+            snapshot_id = block_device['Ebs']['SnapshotId']
+            snapshots.append(snapshot_id)
+    return snapshots
+
+def save_snapshots_to_file(snapshot_ids):
+    with open('snapshot_ids.txt', 'a') as file:
+        for snapshot_id in snapshot_ids:
+            file.write(snapshot_id + '\n')
+
+def deregister_ami(ami_id, profile_name):
+    session = boto3.Session(profile_name=profile_name)
+    ec2_client = session.client('ec2')
+    ec2_client.deregister_image(ImageId=ami_id)
+
+# Read AMI IDs from file
+with open('ami_ids.txt', 'r') as file:
+    ami_ids = [line.strip() for line in file]
+
+# Specify the AWS CLI profile
+aws_profile = 'your_aws_profile'
+
+for ami_id in ami_ids:
+    snapshots = describe_ami_snapshots(ami_id, aws_profile)
+    save_snapshots_to_file(snapshots)
+    deregister_ami(ami_id, aws_profile)
