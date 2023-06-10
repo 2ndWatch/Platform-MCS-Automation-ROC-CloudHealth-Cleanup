4b79092b291260c8051e223aa1a89455f496e49a
Alex <aeversmeyer@2ndwatch.com>
2023-06-09 22:31:50 -0600

    adjust resource entry GUI

--- modules/resource_entry_gui.py
+++ modules/resource_entry_gui.py
@@ -11,8 +11,9 @@ def get_resource_ids_list(client_name, resource_keys, resources_dict, run_date_t
                             f'\n\nClient: {client_name}'
                             f'\nResource: {resource_name}'
                             f'\n\nCopy/paste directly from Excel. Disregard the strange-looking formatting in the '
-                            f'entry field. If there are no resources to enter, leave the entry field blank and click '
-                            f'the <OK> button.',
+                            f'entry field.'
+                            f'\n\nIf there are no resources to enter, leave the entry field blank and click the '
+                            f'<OK> button.',
                             title=f'{client_name} {resource_name} Resource Entry')
         if entry is None:
             sys.exit(0)
@@ -22,8 +23,7 @@ def get_resource_ids_list(client_name, resource_keys, resources_dict, run_date_t
             logger.info(f'\n{resource_name}: {entry_list}')
             resource_ids_list[int(key) - 1] = entry_list
 
-            # Write a file of each resource ID set for backup reference if needed; these files shouldn't be needed by
-            # the application.
+            # Write a file of each resource ID set for backup reference
             file = open(f'{client_name}_{run_date_time}/{client_name} {resource_name}.txt', 'w')
             for i in range(len(entry_list)):
                 if i == (len(entry_list) - 1):
