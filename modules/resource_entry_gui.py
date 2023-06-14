b3ceff9ce916dbe8535d750b16e8b92c9eff471b
Alex <aeversmeyer@2ndwatch.com>
2023-06-13 18:00:10 -0600

    add new resource confirmation logic

--- modules/resource_entry_gui.py
+++ modules/resource_entry_gui.py
@@ -8,45 +8,54 @@ def get_resource_ids(client_name, resource_keys, resources_dict, run_date_time, 
 
     for key in resource_keys:
         resource_name = resources_dict[key]
-        entry = eg.enterbox(f'Enter resource IDs for:'
-                            f'\n\nClient: {client_name}'
-                            f'\nResource: {resource_name}'
-                            f'\n\nCopy/paste directly from Excel. Disregard the strange-looking formatting in the '
-                            f'entry field.'
-                            f'\n\nIf there are no resources to enter, leave the entry field blank and click the '
-                            f'<OK> button.',
-                            title=f'{client_name} {resource_name} Resource Entry')
-        if entry is None:
-            sys.exit(0)
+        should_continue = False
 
-        if entry:
-            entry_list = entry.split('\n')
+        while not should_continue:
+            entry = eg.enterbox(f'Enter resource IDs for:'
+                                f'\n\nClient: {client_name}'
+                                f'\nResource: {resource_name}'
+                                f'\n\nCopy/paste directly from Excel. Disregard the strange-looking formatting in the '
+                                f'entry field.'
+                                f'\n\nIf there are no resources to enter, leave the entry field blank and click the '
+                                f'<OK> button.',
+                                title=f'{client_name} {resource_name} Resource Entry')
+            if entry is None:
+                sys.exit(0)
 
-            confirmed = eg.ynbox(f'You have entered {len(entry_list)} {resource_name} IDs:'
-                                 f'\n\n{os.linesep.join(f"{entry}" for entry in entry_list)}'
-                                 f'\n\nIs this correct?', title='Confirm Resource ID Entry')
+            if entry:
+                entry_list = entry.split('\n')
 
-            # This could either skip the resource, or quit the program; currently skips the resource.
-            if not confirmed:
-                logger.info(f'{resource_name} resources not confirmed. No file written.')
-                # sys.exit(0)
+                confirmed = eg.indexbox(f'You have entered {len(entry_list)} {resource_name} IDs:'
+                                        f'\n\n{os.linesep.join(f"{entry}" for entry in entry_list)}'
+                                        f'\n\nIs this correct?', title='Confirm Resource ID Entry',
+                                        choices=['Yes', 'No', 'Exit'], cancel_choice='Exit')
+
+                # 'Exit' exits the app; 'No' re-prompts for same resource; 'Yes' writes the resource file and continues.
+                if confirmed == 2:
+                    logger.info(f'\nExiting application.')
+                    sys.exit(0)
+                elif confirmed == 1:
+                    logger.info(f'{resource_name} resources not confirmed.')
+                elif confirmed == 0:
+                    logger.info(f'\n{resource_name}: {entry_list}')
+                    resource_ids_list[int(key) - 1] = entry_list
+
+                    # Write a file of each resource ID set for backup reference
+                    try:
+                        file = open(f'{client_name}_{run_date_time}/{client_name} {resource_name}.txt', 'w')
+                        for i in range(len(entry_list)):
+                            if i == (len(entry_list) - 1):
+                                file.write(entry_list[i])
+                            else:
+                                file.write(entry_list[i] + '\n')
+                        file.close()
+                        logger.info(f'"{client_name} {resource_name}.txt" written successfully.')
+                    except FileNotFoundError:
+                        logger.info(f'Something went wrong when trying to write "{client_name} {resource_name}.txt".')
+
+                    should_continue = True
             else:
-                logger.info(f'\n{resource_name}: {entry_list}')
-                resource_ids_list[int(key) - 1] = entry_list
-
-                # Write a file of each resource ID set for backup reference
-                try:
-                    file = open(f'{client_name}_{run_date_time}/{client_name} {resource_name}.txt', 'w')
-                    for i in range(len(entry_list)):
-                        if i == (len(entry_list) - 1):
-                            file.write(entry_list[i])
-                        else:
-                            file.write(entry_list[i] + '\n')
-                    file.close()
-                    logger.info(f'"{client_name} {resource_name}.txt" written successfully.')
-                except FileNotFoundError:
-                    logger.info(f'Something went wrong when trying to write "{client_name} {resource_name}.txt".')
-        else:
-            logger.info(f'{resource_name} resources not entered. No file written.')
+                logger.info(f'{resource_name} resources not entered. No file written.')
+                should_continue = True
 
     return
